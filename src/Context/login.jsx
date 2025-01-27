fdf7ca8cf4be65f3abcc969dfdd59f7683ab8fde
subhanrizwan <subhanahmed444sherazi@gmail.com>
2025-01-27 07:05:38 +0500

    client side to server side

--- src/Context/login.jsx
+++ src/Context/login.jsx
@@ -1,52 +1,34 @@
 import React, { useContext } from "react";
 import UserContext from "./UserContext";
 import { useState } from "react";
-import { TextField } from "@mui/material";
-import axios from 'axios';
-// import 
+
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { setUser } = useContext(UserContext);
 
-  
-  const HandleSubmit = () => {
-    axios.post('/user/order',{
-      username,
-      password,  
-    })
-    .then(res=> console.log(res));
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
   };
-  // const HandleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setUser({ username, password });
-  // };
 
   return (
     <>
-    <form action="">
-    <TextField id="outlined-basic" label="Name" variant="outlined" /> <br /><br />
-    <TextField id="outlined-basic" label="Password" variant="outlined" /> <br /><br />
-    <button onClick={HandleSubmit} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Button
-</button>
-    </form>
-      {/* <h2>Login User</h2>
+      <h2>Login User</h2>
       <input
         type="text"
         placeholder="Username"
         value={username}
-        onChange={(e) =>  setUsername(e.target.value)}
+        onChange={(e) => setUsername(e.target.value)}
       />
-      <br />
 
       <input
         type="text"
         placeholder="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        /> */}
-        {/* <button onClick={HandleSubmit}>Submit</button> */}
+      />
+      <button onClick={HandleSubmit}>Submit</button>
     </>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,54 @@
 import React from "react";
-
+import { useState } from "react";
+import { TextField } from "@mui/material";
+import axios from "axios";
 function Contact() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const { setUser } = useContext(UserContext);
+
+  const HandleSubmit = () => {
+    let User = {
+       username,
+       password
+    }
+    console.log(User);
+    
+    axios.post("http://localhost:3000/order", {
+        User
+      }
+    )
+      .then((res) => console.log("ordered",res))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
-      <h1>Contact</h1>
+        <TextField
+          // id="outlined-basic"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          label="Name"
+          variant="outlined"
+        />{" "}
+        <br />
+        <br />
+        <TextField
+          // id="outlined-basic"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          label="Password"
+          variant="outlined"
+        />{" "}
+        <br />
+        <br />
+        <button
+          onClick={HandleSubmit}
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Button
+        </button>
     </>
   );
 }
